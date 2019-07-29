7925e10cf2853094f9d3219c83a42bae51fdd94a
gaox <781727380@qq.com>
2019-07-29 21:25:05 +0800

    abc

--- src/main/java/com/wxxd/intercepter/intercepter/WebAppConfig.java
+++ src/main/java/com/wxxd/intercepter/intercepter/WebAppConfig.java
@@ -1,14 +1,17 @@
 package com.wxxd.intercepter.intercepter;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
 public class WebAppConfig extends WebMvcConfigurationSupport {
 
     @Override
-    public void addInterceptors(InterceptorRegistry registry){
-        registry.addInterceptor(new Intercepter()).addPathPatterns("/");
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/sh/static/login/**").addResourceLocations("classpath:/static/");
+
+        super.addResourceHandlers(registry);
     }
 }
